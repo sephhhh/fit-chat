4d4bcc3c9f7cbf0b23d7f39429df3260312c17b8
mt3462 <mt3462@drexel.edu>
2023-03-05 18:09:38 +0000

    Update login.py

--- login page/login.py
+++ login page/login.py
@@ -11,13 +11,14 @@ from kivymd.uix.button import MDRectangleFlatButton
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import json
 
 Window.size = (350, 650)
 
 
 class testApp(MDApp):
     def build(self):
-        return Builder.load_file('login.kv')
+        return Builder.load_file('logintest.kv')
 
     global doc_ref
 
@@ -30,13 +31,27 @@ class testApp(MDApp):
     def get_data(self):  # function for submit button
         email = self.root.ids.email.text
         password = self.root.ids.password.text
-        print(email)
-        print(password)
-        doc_ref = firestore.collection("user_login").document(email)
-        doc_ref.set({
-                'email': email,
-                'password': password,
-        })
+        try: #get database data
+            doc_ref = firestore.collection("user_login").document(email)
+            doc = doc_ref.get()
+            y = json.loads(json.dumps(doc.to_dict()))
+            print(y["password"])
+            if str(doc_ref.id) == email and (password == y["password"]):
+                print("Login Successful")
+                self.root.ids.login_message.text = "Successful Login"
+                #move screen function
+            else:
+                print("Invalid Password")
+                self.root.ids.login_message.text = "Invalid Password"
+        except:
+            print(email)
+            print(password)
+            doc_ref = firestore.collection("user_login").document(email)
+            doc_ref.set({
+                    'email': email,
+                    'password': password,
+            })
+            self.root.ids.login_message.text = "Account Created"
 
 if __name__ == '__main__':
     testApp().run()
