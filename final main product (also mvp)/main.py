47ae8ab8df57503674a1516c16262cfad03846fa
mt3462 <mt3462@drexel.edu>
2023-04-21 18:12:04 +0000

    fixed the issue with modifying user information in firebase. also integrated kenix's interest selection with no issue.

--- final main product (also mvp)/main.py
+++ final main product (also mvp)/main.py
@@ -13,7 +13,6 @@ from kivy.uix.button import ButtonBehavior
 from kivy.uix.image import Image
 from functools import partial
 
-
 Window.size = (350, 700)
 
 
@@ -31,7 +30,7 @@ class MyApp(MDApp):
         Builder.load_file('main.kv')
         return MainScreenManager()
 
-    def initializeProfile(self): #function to initialize profile after successful login
+    def initializeProfile(self):  # function to initialize profile after successful login
         accInfo = firebase.get('https://fitchat-d7a73-default-rtdb.firebaseio.com/Users', '')
         name = accInfo[accountKey]['name']
         self.root.ids.name.text = name
@@ -41,8 +40,6 @@ class MyApp(MDApp):
 
         profile = accInfo[accountKey]['profilePicture']
         self.root.ids.profile_picture.source = profile
-
-        pass
 
     def loginScreen(self):
         self.root.current = "createAccount"
@@ -63,45 +60,34 @@ class MyApp(MDApp):
                         global accountKey
                         accountKey = loginInfo
                         self.initializeProfile()
-
                 else:
                     print("Invalid Password")
                     self.root.ids.login_message.text = "Invalid Password"
         except:
-            print(email)
-            print(password)
-
-            data = {
-                'email': email,
-                'password': password,
-                'profilePicture': "fitChatAvatars/defaultAvatar.png",
-                'bio': '"No Biography Written"',
-                'name': "No Name Set",
-            }
-            firebase.post('https://fitchat-d7a73-default-rtdb.firebaseio.com/Users', data)
-            self.root.ids.login_message.text = "Account Created"
+            pass
 
     def createAccount(self):
         email = self.root.ids.newEmail.text
         password = self.root.ids.newPassword.text
-        newBio = self.root.ids.newBio.text
+        bio = self.root.ids.newBio.text
         name = self.root.ids.newName.text
         data = {
             'email': email,
             'password': password,
             'profilePicture': 'fitChatAvatars/defaultAvatar.png',
-            'bio': newBio,
+            'bio': bio,
             'name': name,
         }
         firebase.post('https://fitchat-d7a73-default-rtdb.firebaseio.com/Users', data)
-
+        self.root.current = 'login'
 
     def change_data(self):
         email = self.root.ids.email.text
         password = self.root.ids.password.text
         change_bio = self.root.ids.change_bio.text
         name = self.root.ids.change_name.text
-
+        sport = self.root.ids.sports_label.text
+        accountLink = 'https://fitchat-d7a73-default-rtdb.firebaseio.com/Users/' + accountKey
         accInfo = firebase.get('https://fitchat-d7a73-default-rtdb.firebaseio.com/Users', '')
 
         data = {
@@ -110,8 +96,9 @@ class MyApp(MDApp):
             'profilePicture': accInfo[accountKey]['profilePicture'],
             'bio': change_bio,
             'name': name,
+            'sports': sport
         }
-        firebase.post('https://fitchat-d7a73-default-rtdb.firebaseio.com/Users', data)
+        firebase.patch(accountLink, data)
         self.root.current = 'login'
 
     def change_avatar(self):
@@ -119,14 +106,14 @@ class MyApp(MDApp):
         avatar_grid = self.root.ids.avatar_grid
         for root_dir, folders, files in walk("fitChatAvatars"):
             for f in files:
-                if f != '.DS_Store': #for mac folders
+                if f != '.DS_Store':  # for mac folders
                     img = ImageButton(source="fitChatAvatars/" + f, on_release=partial(self.change_avatar_pic, f))
                     avatar_grid.add_widget(img)
 
     def change_avatar_pic(self, image, widget_id):
         self.root.ids.profile_picture.source = "fitChatAvatars/" + image
 
-        #firebase
+        # firebase
         email = self.root.ids.email.text
         password = self.root.ids.password.text
         bio = self.root.ids.biography.text
@@ -140,8 +127,10 @@ class MyApp(MDApp):
             'bio': bio,
             'name': name,
         }
-        firebase.post('https://fitchat-d7a73-default-rtdb.firebaseio.com/Users', data)
-        #return back
+
+        accountLink = 'https://fitchat-d7a73-default-rtdb.firebaseio.com/Users/' + accountKey
+        firebase.patch(accountLink, data)
+        # return back
         self.root.ids.change_text.text = "Avatar Successfully Changed"
         self.root.current = 'homepage'
 
@@ -150,7 +139,6 @@ class MyApp(MDApp):
     global accountKey
 
     def get_hist(self):
-        email = self.root.ids.email.text
         messages = firebase.get('/Chat', "")
         newMessages = ""
         for i in messages.keys():
@@ -165,6 +153,25 @@ class MyApp(MDApp):
         data['Message'] = message
         firebase.post('https://fitchat-d7a73-default-rtdb.firebaseio.com/Chat', data)
         self.root.ids.Chat.text += "\n" + email + ' said: ' + message
+
+    cred = credentials.Certificate('fitchat-d7a73-firebase-adminsdk-ybqmf-e4babd672a.json')
+    firebase_admin.initialize_app(cred)
+    checks = []
+
+    def check(self, instance, value, sport):
+        if value == True:
+            MyApp.checks.append(sport)
+            sports = ""
+            for i in MyApp.checks:
+                sports = f"{sports} {i}"
+            self.root.ids.sports_label.text = f"You Selected:{sports}"
+        else:
+            MyApp.checks.remove(sport)
+            sports = ""
+            for i in MyApp.checks:
+                sports = f"{sports} {i}"
+            self.root.ids.sports_label.text = f"{sports}"
+
 
 if __name__ == '__main__':
     MyApp().run()
